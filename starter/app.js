82ea35eece7587dfa4cf5b1e7cb7ff4dbc6107d2
rbhogal <rbhogal23gmail.com>
2020-09-29 11:10:35 -0700

    reading input data

--- starter/app.js
+++ starter/app.js
@@ -9,17 +9,47 @@ var budgetController = (function() {
 // UI CONTROLLER
 var UIController = (function() {
 
-    // Some code
+    var DOMstrings = {
+        inputType: '.add__type',
+        descriptionType: '.add__description',
+        valueType: '.add__value',
+        inputBtn: ".add__btn"
+    }
+
+ /* 
+ This (below) is an "empty" object. It is assigned to the variable UIController (UIController.getinput()). Part of a bigger concept called the module pattern. 
+ Returns the object assigned to UIController. This is a closure as well. 
+
+ */
+    return {                           
+        getinput: function() {
+            return {
+                type: document.querySelector(DOMstrings.inputType).value, // Will be either inc or exp
+                description: document.querySelector(DOMstrings.descriptionType).value,
+                value: document.querySelector(DOMstrings.valueType).value
+            }
+
+        },
+
+        getDOMstrings: function() {
+            return DOMstrings;
+        }
+            
+    }
 
 })();
 
 
 // GLOBAL APP CONTROLLER
 var controller = (function(budgetCtrl, UICtrl) {
-        
+    
+    var DOM = UICtrl.getDOMstrings();
+
     var ctrlAddItem = function() {
 
         // 1. Get the field input data
+        var input = UIController.getinput();
+        console.log(input);
 
         // 2. Add the item to the budget controller 
 
@@ -33,7 +63,7 @@ var controller = (function(budgetCtrl, UICtrl) {
 
     }
 
-    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
 
     document.addEventListener('keypress', function(event) {
 
