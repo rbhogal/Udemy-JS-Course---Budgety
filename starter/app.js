e45021de208753fe71ff798b2ea8fa2de6aa553d
rbhogal <rbhogal23gmail.com>
2020-09-30 14:01:46 -0700

    adding new item to UI progress 1

--- starter/app.js
+++ starter/app.js
@@ -78,8 +78,10 @@ var UIController = (function() {
         inputType: '.add__type',
         descriptionType: '.add__description',
         valueType: '.add__value',
-        inputBtn: ".add__btn"
-    }
+        inputBtn: ".add__btn",
+        incomeContainer: '.income__list',
+        expensesContainer: '.expenses__list'
+    };
 
  /* 
  This (below) is an "empty" object. It is assigned to the variable UIController (UIController.getinput()). Part of a bigger concept called the module pattern. 
@@ -96,22 +98,30 @@ var UIController = (function() {
         },
 
         addListItem: function(obj, type) {
-            var html;
+            var html, newHtml;
 
             // Create HTML string with placeholder text
             
 
-        if (type === 'inc') {
-            html = '<div class="item clearfix" id="income-%id%"> <div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
-        } else if (type = 'exp') {
-            html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
-        }
+            if (type === 'inc') {
+                element = DOMstrings.incomeContainer;
+
+                html = '<div class="item clearfix" id="income-%id%"> <div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            } else if (type = 'exp') {
+                element = DOMstrings.expensesContainer;
+                
+                html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+            }
 
             // Replace the placeholder text with some actual data
-
+            newHtml = html.replace('%id%', obj.id);
+            newHtml = newHtml.replace('%description%', obj.description);
+            newHtml = newHtml.replace('%value%', obj.value); 
 
             // Insert the HTML into the DOM
-        }
+
+
+        },
 
         getDOMstrings: function() {
             return DOMstrings;
