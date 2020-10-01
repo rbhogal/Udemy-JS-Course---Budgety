40e6f7b2fdcbd9c3c549287d961eab4bc369959d
rbhogal <rbhogal23gmail.com>
2020-09-30 17:11:09 -0700

    add item to the UI completed

--- starter/app.js
+++ starter/app.js
@@ -98,7 +98,7 @@ var UIController = (function() {
         },
 
         addListItem: function(obj, type) {
-            var html, newHtml;
+            var html, newHtml, element;
 
             // Create HTML string with placeholder text
             
@@ -109,7 +109,7 @@ var UIController = (function() {
                 html = '<div class="item clearfix" id="income-%id%"> <div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
             } else if (type = 'exp') {
                 element = DOMstrings.expensesContainer;
-                
+
                 html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
             }
 
@@ -119,7 +119,7 @@ var UIController = (function() {
             newHtml = newHtml.replace('%value%', obj.value); 
 
             // Insert the HTML into the DOM
-
+            document.querySelector(element).insertAdjacentHTML('beforeEnd', newHtml);
 
         },
 
@@ -159,6 +159,7 @@ var controller = (function(budgetCtrl, UICtrl) {
         newItem = budgetCtrl.addItem(input.type, input.description, input.value);
 
         // 3. Add the item to the UI
+        UICtrl.addListItem(newItem, input.type);
 
         // 4. Calculate the budget
 
