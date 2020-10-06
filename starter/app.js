7a283c5bcb8152201c03a877715764640337919b
rbhogal <rbhogal23gmail.com>
2020-10-06 12:35:57 -0700

    Delete item from the UI

--- starter/app.js
+++ starter/app.js
@@ -195,6 +195,11 @@ var UIController = (function() {
 
         },
 
+        deleteListItem: function(selectorID) {
+            var el = document.getElementById(selectorID);
+            el.parentNode.removeChild(el);
+        },
+
         // Clear HTML fields
         clearFields: function() {
             var fields, fieldsArr;
@@ -302,9 +307,12 @@ var controller = (function(budgetCtrl, UICtrl) {
 
             // 1. Delete the item from the data structure
             budgetCtrl.deleteItem(type, ID);
-            // 2. Delete the item from the UI
 
+            // 2. Delete the item from the UI
+            UICtrl.deleteListItem(itemID);
+            
             // 3. Update and show the new budget
+            updateBudget();
 
         }
 
