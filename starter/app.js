a91c9cf656b295d35b7de265124c2a95fdf4dcba
rbhogal <rbhogal23gmail.com>
2020-10-09 12:16:33 -0700

    fix type border box issue. Set radius

--- starter/app.js
+++ starter/app.js
@@ -224,6 +224,11 @@ var UIController = (function() {
         return (type === 'exp' ? sign = '-' : sign = '+') + ' ' + int + "." + dec; 
     };
 
+    var nodeListForEach = function(list, callback) {
+        for (var i = 0; i < list.length; i++) {
+            callback(list[i], i);
+        }
+    };
 
  /* 
  This (below) is an "empty" object. It is assigned to the variable UIController (UIController.getinput()). Part of a bigger concept called the module pattern. 
@@ -306,12 +311,6 @@ var UIController = (function() {
 
             var fields = document.querySelectorAll(DOMstrings.expensesPercLabel);
 
-            var nodeListForEach = function(list, callback) {
-                for (var i = 0; i < list.length; i++) {
-                    callback(list[i], i);
-                }
-            };
-
             nodeListForEach(fields, function(current, index) {
 
                 if (percentages[index] > 0) {
@@ -334,6 +333,20 @@ var UIController = (function() {
 
             year = now.getFullYear(); 
             document.querySelector(DOMstrings.dateLabel).textContent = months[month] + ' ' + year;         
+        },
+
+        changedType: function() {
+
+            var fields = document.querySelectorAll(
+                DOMstrings.inputType + ',' + 
+                DOMstrings.inputDescription + ',' + 
+                DOMstrings.inputValue
+                );
+
+            nodeListForEach(fields, function(cur) {
+                cur.classList.toggle('red-focus');
+            });
+
         },
 
         getDOMstrings: function() {
@@ -359,6 +372,8 @@ var controller = (function(budgetCtrl, UICtrl) {
         }); 
 
         document.querySelector(DOM.container).addEventListener('click', ctrlDeleteItem);
+
+        document.querySelector(DOM.inputType).addEventListener('change', UICtrl.changedType);
 
     };
 
